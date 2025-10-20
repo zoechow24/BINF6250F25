7a1e96b79f64ba4c0da5e675bfc769f4ffa58c61
michael-bambha <bambha.m@northeastern.edu>
2025-10-20 13:20:07 -0600

    added JC and K2M models into the smith_waterman file. Added functions for calculating transition proportions and p-distances also. Also added in my original unedited smith_waterman function which returns the alignment, not just the score (needed for JC and K2P).

--- Project06/smith_waterman.py
+++ Project06/smith_waterman.py
@@ -4,6 +4,9 @@ Description: Small Python module for finding
 the maximum score of a Smith-Waterman alignment
 """
 from enum import Enum
+from itertools import filterfalse
+import math
+from typing import Tuple
 import numpy as np
 
 class Direction(Enum):
@@ -65,6 +68,119 @@ def cal_score(
     max_direction, max_score = max(score_dict.items(), key=lambda score: score[1])
     return max_direction, max_score
 
+def traceback(
+    seq1: str, seq2: str, traceback_matrix: np.ndarray, maximum_position: int
+):
+    """Find the optimal path through scoring marix
+
+        diagonal: match/mismatch
+        up: gap in seq1
+        left: gap in seq2
+
+    Args:
+        seq1 (str) : First sequence being aligned
+        seq2 (str) : Second sequence being aligned
+        traceback_matrix (numpy array): traceback matrix
+        maximum_position (tuple): starting position to trace back from
+
+    Returns:
+        aligned_seq1 (str): e.g. GTTGAC
+        aligned_seq2 (str): e.g. GTT-AC
+
+    Pseudocode:
+
+        while current_move != END:
+            current_move = traceback_matrix[current_row][current_col]
+            if current_move == DIAG:
+               ...
+            elif current_move == UP:
+                ...
+            elif current_move == LEFT:
+                ...
+
+               base1 =  seq1[current_row] | base2 = seq2[current_col]
+
+        aligned_seq1 = aligned_seq1[::-1]
+    """
+    aligned_seq1 = ""
+    aligned_seq2 = ""
+    current_move = traceback_matrix[maximum_position]
+    current_row, current_col = maximum_position
+
+    while current_move != Direction.END:
+
+        if current_move == Direction.DIAG:
+            aligned_seq1 += seq1[current_row - 1]
+            aligned_seq2 += seq2[current_col - 1]
+            current_row -= 1
+            current_col -= 1
+
+        elif current_move == Direction.LEFT:
+            aligned_seq1 += seq1[current_row - 1]
+            aligned_seq2 += "-"
+            current_col -= 1
+
+        elif current_move == Direction.UP:
+            aligned_seq1 += "-"
+            aligned_seq2 += seq2[current_col - 1]
+            current_row -= 1
+
+        current_move = traceback_matrix[current_row, current_col]
+
+    return aligned_seq1[::-1], aligned_seq2[::-1]
+
+
+def smith_waterman(
+    seq1: str,
+    seq2: str,
+    match_score: int = 1,
+    mismatch_score: int = -1,
+    gap_score: int = -1,
+):
+    """Smith-Waterman algorithm for local alignment
+
+    Args:
+        seq1 (str): input seq 1
+        seq2 (str): input seq 2
+        match: default = +1
+        mismatch: default = -1
+        gap: default = -1
+
+    Returns:
+        aligned_seq1 (str)
+        aligned_seq2 (str)
+        score_matrix (numpy array): scoring matrix
+    """
+    rows = len(seq1) + 1
+    cols = len(seq2) + 1
+
+    score_matrix = np.zeros((rows, cols))
+    # using an object array isn't necessarily efficient, but is readable
+    # since we aren't really using np vectorization methods, shouldn't matter
+    # if performance was a concern, we could use IntEnum instead
+    traceback_matrix = np.full((rows, cols), Direction.END, dtype=object)
+
+    max_score = 0
+    max_pos = (0, 0)
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            direction, score = cal_score(
+                score_matrix, seq1, seq2, i, j, match_score, mismatch_score, gap_score
+            )
+            score_matrix[i, j] = score
+            traceback_matrix[i, j] = direction
+
+            if score > max_score:
+                max_score = score
+                max_pos = (i, j)
+
+    aligned_seq1, aligned_seq2 = traceback(
+        seq1, seq2, traceback_matrix, maximum_position=max_pos
+    )
+
+    return aligned_seq1, aligned_seq2, score_matrix
+
 
 def smith_waterman_max_score(
     seq1: str,
@@ -111,3 +227,86 @@ def smith_waterman_max_score(
                 # max_pos = (i, j)
 
     return max_score
+
+
+def calculate_p_distance(seq1: str, seq2: str) -> float:
+    """Calculate the p-distance between two aligned sequences.
+
+    Args:
+        seq1 (str): First aligned sequence 1
+        seq2 (str): Second aligned sequence
+
+    Returns:
+        float: p distance between 0 and 1
+    """
+    valid_pairs = ((a, b) for a, b in zip(seq1, seq2) if "-" not in (a, b))
+    mismatches = total = 0
+    for a, b in valid_pairs:
+        total += 1
+        mismatches += a != b
+    return mismatches / total if total else 0.0
+
+
+def jukes_cantor(p_dist: float) -> float:
+    """Corrects p-distances based on Jukes-Cantor evolutionary model.
+
+    Args:
+        p_dist (float): p-distance between two sequences
+
+    Returns:
+        float: Corrected p-distances based on JC
+    """
+    if p_dist >= 0.75:
+        return float('inf')
+    return (-3/4) * math.log(1 - (4 * p_dist / 3))
+
+
+def calculate_transition_proportions(seq1: str, seq2: str) -> Tuple[float, float]:
+    """Find the transition and transversion proportions of two sequences.
+
+    Args:
+        seq1 (str): Aligned sequence 1
+        seq2 (str): Aligned sequence 2
+
+    Returns:
+        Tuple[float, float]: Tuple of (transition, transversion) proportions.
+    """
+    purines = {'A', 'G'}
+    pyrimidines = {'C', 'T'}
+
+    pairs = filterfalse(lambda ab: '-' in ab, zip(seq1.upper(), seq2.upper()))
+
+    transitions = transversions = valid_sites = 0
+
+    for a, b in pairs:
+        valid_sites += 1
+        if a == b:
+            continue
+        if (a in purines and b in purines) or (a in pyrimidines and b in pyrimidines):
+            transitions += 1
+        else:
+            transversions += 1
+
+    if valid_sites == 0:
+        return 0.0, 0.0
+
+    return transitions / valid_sites, transversions / valid_sites
+
+
+def kimura_two_parameter(aligned_seq1: str, aligned_seq2: str) -> float:
+    """Find the K2P distance based on proportion of
+    transitions and transversions from two sequences.
+
+    Args:
+        aligned_seq1 (str): Aligned sequence 1
+        aligned_seq2 (str): Aligned sequence 2
+
+    Returns:
+        float: K2P corrected p-distances
+    """
+    P, Q = calculate_transition_proportions(aligned_seq1, aligned_seq2)
+    if (1 - 2*P - Q) <= 0 or (1 - 2*Q) <= 0:
+        return float('inf')
+    return -0.5 * math.log(1 - 2*P - Q) - 0.25 * math.log(1 - 2*Q)
+
+    
